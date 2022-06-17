14465508dd0e815cd15043508cffd4527b7c35bf
smulrow <smulrow@gmail.com>
2022-06-17 14:55:21 -0700

    added alert

--- Develop/script.js
+++ Develop/script.js
@@ -36,16 +36,18 @@ if (passwordSpecial === true) {
   newPassword += special
 }
 
+if (passwordUppercase != true && passwordLowercase != true && passwordSpecial != true && passwordNumber != true){
+  alert("At least one character type must be selected")
+  return;
+}
+
 for (var i = 0; i < passwordLength; i++){
    var indexChar = Math.floor(Math.random() * newPassword.length)
     var selectedChar = newPassword[indexChar]
     password+=selectedChar
- // select a random character for each time we add to the length 
- // add it to the password string 
- // 
  }
 return password;
-// return password 
+ 
 }
 
 
@@ -63,27 +65,3 @@ function writePassword(event) {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or //special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be /selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
-
-// need prompts for password length 8-128
-// lowercase, uppercase, numbers, special characters 
-// if user selects # outside of range, restart process
-// if user doesnt select at least one character type, restart process
-// NEED and array to iterate over to get password characters
-//
